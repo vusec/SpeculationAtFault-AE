d299ee7d9340373ebe33e318a7444723042f5938
Emanuele <emanuele.vannacci@gmail.com>
2023-06-20 23:39:10 +0200

    exp6 create subdir

--- intel/experiment_6/vs-ni-assist/vspec-ni-assist-accessed.sh
+++ intel/experiment_6/vs-ni-assist/vspec-ni-assist-accessed.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-contract="vspec-NI"
+contract="CT-VS-NI"
 
 SCRIPT=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT)
 TIMEOUT=$(( 24 * 3600 ))
 
-WORK_DIR=$(realpath $SCRIPT_DIR/../../)
+WORK_DIR=$(realpath $SCRIPT_DIR/../../..)
 echo "HOME=$WORK_DIR"
 
 
@@ -17,11 +17,6 @@ timestamp=$(date '+%y-%m-%d-%H-%M')
 
 results=$SCRIPT_DIR/results
 mkdir -p $results
-
-cp $SCRIPT_DIR/assist-accessed.yaml $SCRIPT_DIR/assist-accessed-NI.yaml
-echo ""                               >> $SCRIPT_DIR/assist-accessed-NI.yaml
-echo "contract_execution_clause:"     >> $SCRIPT_DIR/assist-accessed-NI.yaml
-echo "  - nullinj-assist"    >> $SCRIPT_DIR/assist-accessed-NI.yaml
 
 logfile="$SCRIPT_DIR/results/assist-accessed-dh-$timestamp.log"
 echo "[+] Fuzzing ucode-assist (accessed bit) with $contract; Log at $logfile"
