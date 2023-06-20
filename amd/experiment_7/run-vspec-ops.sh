bc4a6290bf8c73f4b74a2da3ac2de28ddd47eb19
Emanuele <emanuele.vannacci@gmail.com>
2023-06-20 16:18:08 +0200

    fix exp7 and exp8

--- amd/experiment_7/run-vspec-ops.sh
+++ amd/experiment_7/run-vspec-ops.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-./run-div-overflow.sh
-./run-div-zero.sh
+set -e
+
+contract="vspec-ops"
+
+SCRIPT=$(realpath $0)
+SCRIPT_DIR=$(dirname $SCRIPT)
+TIMEOUT=$(( 24 * 3600 ))
+
+WORK_DIR=$(realpath $SCRIPT_DIR/../../)
+echo "HOME=$WORK_DIR"
+
+
+instructions="$WORK_DIR/base.json"
+timestamp=$(date '+%y-%m-%d-%H-%M')
+
+results=$SCRIPT_DIR/results
+mkdir -p $results
+
+logfile="$SCRIPT_DIR/results/vspec-ops-div-zero-$timestamp.log"
+echo "[+] Fuzzing #DE (divide-by-zero) with $contract; Log at $logfile"
+rvzr fuzz -s $instructions -c  $SCRIPT_DIR/vspec-ops-div-zero.yaml -i 100 -n 100000000 --timeout $TIMEOUT  -w $SCRIPT_DIR/results/violations/ &> $logfile
--- amd/experiment_7/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-./vspec-all-div-overflow.sh
-./vspec-all-div-zero.sh
