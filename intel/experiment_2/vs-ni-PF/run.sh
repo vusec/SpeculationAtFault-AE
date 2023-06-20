0c821dddaf9084fa77f0b9d08fb0b658d1027f1c
Emanuele <emanuele.vannacci@gmail.com>
2023-06-20 22:52:35 +0200

    intel exp2 create dir for vs-ni

---
+++ intel/experiment_2/vs-ni-PF/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+./vs-ni-PF-present.sh
+./vs-ni-PF-ro.sh
+./vs-ni-PF-smap.sh
---
+++ intel/experiment_2/vs-ni-PF/vs-ni-PF-smap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+contract="CT-VS-NI"
+
+SCRIPT=$(realpath $0)
+SCRIPT_DIR=$(dirname $SCRIPT)
+TIMEOUT=$(( 24 * 3600 ))
+
+WORK_DIR=$(realpath $SCRIPT_DIR/../../)
+echo "HOME=$WORK_DIR"
+
+
+instructions="$WORK_DIR/base.json"
+timestamp=$(date '+%y-%m-%d-%H-%M')
+
+results=$SCRIPT_DIR/results
+mkdir -p $results
+
+logfile="$SCRIPT_DIR/results/PF-smap-$timestamp.log"
+echo "[+] Fuzzing #PF (smap) with $contract; Log at $logfile"
+rvzr fuzz -s $instructions -c  $SCRIPT_DIR/vs-ni-PF-smap.yaml -i 100 -n 100000000 --timeout $TIMEOUT  -w $SCRIPT_DIR/results/violations/ &> $logfile
